0f8dbd540cb47df7d892146ed18408b505c7a54c
huangchao-netease <huangchao_ntes@126.com>
2020-11-04 16:22:28 +0800

    添加签名生成方法的测试

--- java-sms-demo/src/main/java/com/netease/is/sms/demo/Main.java
+++ java-sms-demo/src/main/java/com/netease/is/sms/demo/Main.java
@@ -6,6 +6,12 @@
 
 package com.netease.is.sms.demo;
 
+import com.netease.is.sms.demo.utils.ParamUtils;
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @author huangchao04
  * @version 2020-11-03
@@ -22,5 +28,25 @@ public class Main {
         // OtpVerifyDemo.sendAndVerifyOtp();
 
         System.out.println("示例执行完毕。");
+    }
+
+    private static void testSignature() {
+        String secretKey = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
+        Map<String, String> params = new HashMap<>();
+        params.put("nonce", "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
+        params.put("timestamp", "1598889600000");
+        params.put("version", "v2");
+        params.put("secretId", "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
+        params.put("businessId", "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
+        params.put("templateId", "xxxxx");
+        params.put("mobile", "xxxxxxxxxxx");
+        params.put("paramType", "json");
+        params.put("params", "{\"code\":\"123456\"}");
+
+        String expected = "22d28d2ce543e8c97bd12f078a30784b";
+
+        String sign = ParamUtils.genSignature(secretKey, params);
+
+        assert StringUtils.equals(expected, sign);
     }
 }
